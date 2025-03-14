9721883c3bff72e60c946b3b0eac82d8922211ea
Talha <mohammad.talha@argonteq.com>
2025-03-15 01:11:40 +0500

    build

--- src/redux/etherApi.js
+++ src/redux/etherApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const etherApi = axios.create({
-  baseURL: "http://192.168.100.21:8545",
+  baseURL: "http://10.1.10.146:8545",
   //baseURL : "https://testnet-rpc-seoul.gen.foundation",
   headers: { "content-type": "application/json" },
 });
