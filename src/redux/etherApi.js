42daa9faa13bde030bf017ea7707efc195c8382a
Talha <62717627+talhajaami@users.noreply.github.com>
2025-06-05 16:34:26 +0500

    update rpc

--- src/redux/etherApi.js
+++ src/redux/etherApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const etherApi = axios.create({
-  baseURL: "http://10.1.10.146:8545",
+  baseURL: "https://scbnledger.com",
   //baseURL : "https://testnet-rpc-seoul.gen.foundation",
   headers: { "content-type": "application/json" },
 });
